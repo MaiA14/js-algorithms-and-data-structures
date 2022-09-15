7d73ca228a981277aa0fc368b310bd6142a3310f
mai aloni <maiaa1993@gmail.com>
2022-09-15 16:56:10 +0300

    pq

--- data-structures/priorityQueue.js
+++ data-structures/priorityQueue.js
@@ -1,4 +1,4 @@
-class MaxBinaryHeap {
+class PriorityQueue {
     constructor() {
         this.values = [];
     }
@@ -9,7 +9,7 @@ class MaxBinaryHeap {
         while (true) {
             let parentIndex = Math.floor((index - 1) / 2);
             let parent = this.values[parentIndex];
-            if (element <= parent) {
+            if (element.priority >= parent.priority) {
                 break;
             }
             this.values[parentIndex] = value;
@@ -30,14 +30,14 @@ class MaxBinaryHeap {
             let swap = null;
             if (leftChildIndex < length) {
                 leftChild = this.values[leftChildIndex];
-                if (leftChild > value) {
+                if (leftChild.priority < value.priority) {
                     swap = leftChildIndex;
                 }
             }
             if (rightChildIndex < length) {
                 rightChild = this.values[rightChildIndex];
-                if (swap === null && rightChild > value ||
-                    swap === null && rightChild > leftChild) {
+                if (swap === null && rightChild.priority < value.priority ||
+                    swap === null && rightChild.priority < leftChild.priority) {
                     swap = rightChildIndex;
                 }
             }
@@ -51,21 +51,31 @@ class MaxBinaryHeap {
         }
     }
 
-    extractMax() {
-        const max = this.values[0];
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+
+    dequeue() {
+        const min = this.values[0];
         const end = this.values.pop();
         this.values[0] = end;
         if (this.values.length > 0) {
             this.sinkDown();
-            return max;
+            return min;
         }
-    }
-
-    insert(value) {
-        this.values.push(value);
-        this.bubbleUp();
     }
 }
 
-let heap = new MaxBinaryHeap([41, 39, 33, 18, 27, 12]);
-heap.insert(55);
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+let pq = new PriorityQueue();
+pq.enqueue("common cold", 5);
+pq.enqueue("gunshot cold", 1);
+pq.enqueue("high fever", 4);
